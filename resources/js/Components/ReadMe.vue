8a1c6be7dcd5702464a0c0b8fcbcff5e62fd68ec
jack <43416057+member87@users.noreply.github.com>
2022-07-30 23:06:15 +0100

    added more responsiviness to readme

--- resources/js/Components/ReadMe.vue
+++ resources/js/Components/ReadMe.vue
@@ -14,7 +14,7 @@ let markdown = ref("");
 
 <template>
   <div
-    class="mr-0 lg:mr-3 prose prose-sm max-w-full border border-dark-accent/60 rounded shadow p-10 prose-invert md:prose-base">
+    class="mr-0 lg:mr-3 prose p-0 prose-sm max-w-full border-0 border-dark-accent/60 rounded shadow prose-invert sm:border sm:p-5 md:prose-base md:p-10">
     <span v-html="markdown"></span>
   </div>
 </template>
